9e7f325f6b10319d85bfea8f32322b917aeed8b5
pathakroshni19 <roshni_pathak@dlsu.edu.ph>
2026-07-14 18:12:28 +0800

    Update flight.js

--- models/flight.js
+++ models/flight.js
@@ -1,1 +1,40 @@
+const mongoose = require('mongoose');
 
+const flightSchema = new mongoose.Schema({
+    flightNumber: {
+        type: String,
+        required: true
+    },
+    airline: {
+        type: String,
+        required: true
+    },
+    origin: {
+        type: String,
+        required: true
+    },
+    destination: {
+        type: String,
+        required: true
+    },
+    departure: {
+        type: Date,
+        required: true
+    },
+    arrival: {
+        type: Date,
+        required: true
+    },
+    seats: {
+        type: Number,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    }
+});
+
+const Flight = mongoose.model('Flight', flightSchema);
+
+module.exports = Flight;

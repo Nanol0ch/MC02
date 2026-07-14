f6f92e02038abe276feeb74e25cc526c2119b563
pathakroshni19 <roshni_pathak@dlsu.edu.ph>
2026-07-14 17:58:36 +0800

    Delete AustriaPartsMco2 directory

--- AustriaPartsMco2/reservations.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const reservationsController = require('../controller/reservationsController');
-
-router.get('/reservations', reservationsController.getMyReservations);
-router.patch('/reservations/:id/seat', reservationsController.updateSeat);
-router.delete('/reservations/:id', reservationsController.cancelReservation);
-
-module.exports = router;

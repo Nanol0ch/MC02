5c1eab4a6ec45c7d0c272b47e5bf006773cac2d8
Nanol0ch <aaustriacarlos8@gmail.com>
2026-07-15 13:19:46 +0800

    Add files via upload

--- routes/admin.js
+++ routes/admin.js
@@ -1,1 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const adminController = require('../controllers/adminController');
 
+router.get('/admin', adminController.getDashboard);
+router.get('/admin/users', adminController.getUsersPage);
+
+router.get('/admin/flights', adminController.getFlights);
+router.post('/admin/flights', adminController.createFlight);
+router.put('/admin/flights/:id', adminController.updateFlight);
+router.delete('/admin/flights/:id', adminController.deleteFlight);
+
+router.get('/admin/reservations', adminController.getAllReservations);
+router.patch('/admin/reservations/:id/status', adminController.updateReservationStatus);
+
+module.exports = router;
--- routes/reservations.js
+++ routes/reservations.js
@@ -1,1 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const reservationController = require('../controllers/reservationController');
 
+router.get('/reservations', reservationController.getMyReservations);
+router.patch('/reservations/:id/seat', reservationController.updateSeat);
+router.delete('/reservations/:id', reservationController.cancelReservation);
+
+module.exports = router;

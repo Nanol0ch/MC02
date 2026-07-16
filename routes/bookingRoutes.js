95240498abac2f83f3be2c2d5be8cc9a6c8ae919
pathakroshni19 <roshni_pathak@dlsu.edu.ph>
2026-07-16 16:00:42 +0800

    Update bookingRoutes.js

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -1,11 +1,9 @@
-// routes/bookingRoutes.js
 const express = require('express');
 const router = express.Router();
 const bookingController = require('../controllers/bookingController');
 
-// Cleanly map the URL endpoints to your controller functions
-router.get('/booking/:flightId', bookingController.getBookingPage);
+router.get('/:flightId', bookingController.getBookingPage);
 router.get('/api/flights/:id/occupied-seats', bookingController.getOccupiedSeats);
-router.post('/bookings', bookingController.submitBooking);
+router.post('/', bookingController.submitBooking);
 
 module.exports = router;

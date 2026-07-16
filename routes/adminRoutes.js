dd45041104344784590e2dfd4c7af3907b9ce87b
pathakroshni19 <roshni_pathak@dlsu.edu.ph>
2026-07-16 15:59:22 +0800

    Update adminRoutes.js

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -2,15 +2,15 @@ const express = require('express');
 const router = express.Router();
 const adminController = require('../controllers/adminController');
 
-router.get('/admin', adminController.getDashboard);
-router.get('/admin/users', adminController.getUsersPage);
+router.get('/', adminController.getDashboard);
+router.get('/users', adminController.getUsersPage);
 
-router.get('/admin/flights', adminController.getFlights);
-router.post('/admin/flights', adminController.createFlight);
-router.put('/admin/flights/:id', adminController.updateFlight);
-router.delete('/admin/flights/:id', adminController.deleteFlight);
+router.get('/flights', adminController.getFlights);
+router.post('/flights', adminController.createFlight);
+router.put('/flights/:id', adminController.updateFlight);
+router.delete('/flights/:id', adminController.deleteFlight);
 
-router.get('/admin/reservations', adminController.getAllReservations);
-router.patch('/admin/reservations/:id/status', adminController.updateReservationStatus);
+router.get('/reservations', adminController.getAllReservations);
+router.patch('/reservations/:id/status', adminController.updateReservationStatus);
 
 module.exports = router;

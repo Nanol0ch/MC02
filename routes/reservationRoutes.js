c719609fb8bcb8773e4054a7b8e0183a432eeb98
pathakroshni19 <roshni_pathak@dlsu.edu.ph>
2026-07-17 05:08:10 +0800

    Remove flight and reservation management routes
    Removed flight and reservation management routes from the reservationRoutes file.

--- routes/reservationRoutes.js
+++ routes/reservationRoutes.js
@@ -6,12 +6,4 @@ router.get('/', reservationController.getMyReservations);
 router.patch('/:id/seat', reservationController.updateSeat);
 router.delete('/:id', reservationController.cancelReservation);
 
-router.get('/flights', adminController.getFlights);
-router.post('/flights', adminController.createFlight);
-router.put('/flights/:id', adminController.updateFlight);
-router.delete('/flights/:id', adminController.deleteFlight);
-
-router.get('/reservations', adminController.getAllReservations);
-router.patch('/reservations/:id/status', adminController.updateReservationStatus);
-
 module.exports = router;
